2e2599304e4a14f8176606ba128ea0676a923745
Владимир Мелихов <b1esssed1@yandex.com>
2025-10-20 19:56:56 +0300

    version6

--- config/urls.py
+++ config/urls.py
@@ -1,15 +1,13 @@
-"""
-URL configuration for mvs-movie project.
-"""
-
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('apps.accounts.urls', namespace='accounts')),
+    path('', include('apps.movies.urls', namespace='movies')),
 ]
 
 if settings.DEBUG:
