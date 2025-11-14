1796c14254348283c8897a548c1cadc516ff7462
root <root@vmi2891200.contaboserver.net>
2025-11-14 07:36:43 +0100

    final version for prod

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -4,8 +4,6 @@ import os
 bind = "0.0.0.0:8000"
 backlog = 2048
 
-# Workers
-# workers = multiprocessing.cpu_count() * 2 + 1
 workers = 4
 worker_class = "sync"
 worker_connections = 1000
@@ -14,23 +12,16 @@ max_requests_jitter = 50
 timeout = 120
 keepalive = 5
 
-# Логирование
 accesslog = "-"
 errorlog = "-"
 loglevel = "info"
 access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s" %(D)s'
 
-# Process naming
 proc_name = "videohub_gunicorn"
 
-# Server mechanics
 daemon = False
 pidfile = None
 umask = 0
 user = None
 group = None
 tmp_upload_dir = None
-
-# SSL (если нужно на уровне Gunicorn, но у нас SSL на Nginx)
-# keyfile = None
-# certfile = None
